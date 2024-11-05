56b4a672ad572e508581ab74e7274fa46470dd95
akash <akashsahanwu089@gmail.com>
2024-11-06 01:58:59 +0600

    Now

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-import Card from './components/Card';
-import Data from './data.json';
-
-function App() {
-  return (
-    <div>
-      <h1 className="headingStyle">Todo App</h1>
-      {Data.map((item, index) => (
-        <Card key={index} titleText={item.title} cardDesc={item.desc} />
-      ))}
-    </div>
-  );
+import List from './components/UniqueList/List';
+export default function App() {
+  return <List />;
 }
-
-export default App;
---
+++ src/components/UniqueList/List.jsx
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    title: 'Todo1',
+    desc: 'Todo1 description 1',
+  },
+  {
+    title: 'Todo2',
+    desc: 'Todo2 description 2',
+  },
+  {
+    title: 'Todo3',
+    desc: 'Todo3 description 3',
+  },
+];
+
+const List = () => {
+  return <div>List</div>;
+};
+export default List;
