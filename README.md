e5b0e54ec0c3fd5331b36888b49bedade82923cc
AkashItlogiko <akashsahanwu089@gmail.com>
2024-11-06 12:39:48 +0600

    uniqueID

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
- 
+# mapping data with a unique id
+
+[![Mapping with unique id]](https://youtu.be/Dj7ynTdhy1Q?si=lWMjPNNTauoOFsfc)
+
+## ay jayga te mara unique id create jonna `uuid` pakage used korace ar por proti ta id jonna unique id create hoyasa.
--- src/components/UniqueList/List.jsx
+++ src/components/UniqueList/List.jsx
@@ -1,19 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const todos = [
   {
+    id: uuidv4(),
     title: 'Todo1',
     desc: 'Todo1 description 1',
   },
   {
+    id: uuidv4(),
     title: 'Todo2',
     desc: 'Todo2 description 2',
   },
   {
+    id: uuidv4(),
     title: 'Todo3',
     desc: 'Todo3 description 3',
   },
 ];
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div>
+      {todos.map(todo => {
+        const { id, title, desc } = todo;
+        return (
+          <div key={id}>
+            <h3>{title}</h3>
+            <p>{desc}</p>
+            <br></br>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default List;
