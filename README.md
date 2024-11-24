e0948121056c35ee9c18739527166f307d7cee01
AkashItlogiko <akashsahanwu089@gmail.com>
2024-11-24 15:23:36 +0600

    ok

---
+++ README.md
@@ -0,0 +1,3 @@
+# A complete Todo App
+
+[![A complete Todo App]](https://www.youtube.com/watch?v=qQk4bwj5_ts&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=35)
--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -10,6 +10,11 @@ const Todo = props => {
         <h3>{title}</h3>
         <p>{desc}</p>
       </div>
+      <div>
+        <button>
+          <i className="fa fa-trash fa-2x"></i>
+        </button>
+      </div>
     </article>
   );
 };
