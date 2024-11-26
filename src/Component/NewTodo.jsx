66b1bbc85a923521425f866b04aed81b01a79653
AkashItlogiko <akashsahanwu089@gmail.com>
2024-11-26 18:12:46 +0600

    newTodo

--- src/Component/NewTodo.jsx
+++ src/Component/NewTodo.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import style from './neeTodo.module.css';
+
 const NewTodo = () => {
   return (
     <form>
       <div>
         <label htmlFor="title">Title: </label>
-        <input type="text" id="title" name="title" id="" />
+        <input type="text" id="title" name="title" />
       </div>
+      <div>
+        <label htmlFor="desc">Title: </label>
+        <textarea type="text" id="desc" name="desc" />
+      </div>
+      <button type="submit">Add Todo</button>
     </form>
   );
 };
