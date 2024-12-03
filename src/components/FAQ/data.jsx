05b4c8e903cbc7744b674afe7ca22adf721fc7e2
AkashItlogiko <akashsahanwu089@gmail.com>
2024-12-03 19:19:31 +0600

    now

--- src/components/FAQ/data.jsx
+++ src/components/FAQ/data.jsx
@@ -1,7 +1,7 @@
 export const faqsData = [
   {
     id: 1,
-    title: 'How can we reach you?',
+    title: 'How can we reach a you?',
     desc: 'Adding an FAQ section to individual webpages saves the website visitor from having to click on an FAQ section and then taking an additional step to navigate to the correct question and answer.',
   },
   {
