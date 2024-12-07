5b8759f366153b9eb98195a89615bcbd91c7330b
AkashItlogiko <akashsahanwu089@gmail.com>
2024-12-07 16:11:12 +0600

    FAQ completed

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './faq.module.css'
 
-const FAQ = () => {
+const FAQ = ({id,title,desc}) => {
+    const[toggle,setToggle]=useState(false);
     return (
+     <article className={style.faq}>
         <div>
-            
+            <h4>{title}</h4>
+            <button onClick={()=>{
+                setToggle(!toggle)
+            }}>
+                   {toggle ? '-':'+'}     
+            </button>
+          
         </div>
+        {toggle && <p>{desc}</p>}  
+     </article>
     );
 };
 
--- src/components/FAQ/data.jsx
+++ src/components/FAQ/data.jsx
@@ -14,4 +14,9 @@ export const faqsData = [
     title: 'How can we reach you?',
     desc: 'Adding an FAQ section to individual webpages saves the website visitor from having to click on an FAQ section and then taking an additional step to navigate to the correct question and answer.',
   },
+  {
+    id: 4,
+    title: 'How can we reach you?',
+    desc: 'Adding an FAQ section to individual webpages saves the website visitor from having to click on an FAQ section and then taking an additional step to navigate to the correct question and answer.',
+  }
 ];
