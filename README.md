ec31f1d5cad019739b59438ece43f85ca66d3327
AkashItlogiko <akashsahanwu089@gmail.com>
2024-12-09 18:54:17 +0600

    error-handleing

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Fetching data with useEffect hook
+# how to handle the error and add loading status
 
-## API call korte hole amader definetly useEffect used korte hobe.
+##
 
-## 1st= Ay jayga amra useEffect maje fetch used korace tar por ay fetch url thaki amra `respoced`pabo tar por say responc ka amra json convart kore niyace tar por say jason data ka amra todos name jay useState ashe say jayga set kore nirce setTodos dara.
-
-[! fetching data](https://www.youtube.com/watch?v=Z-EkslDJTJI&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=42)
+[! how to handle the error and add loading status data](https://www.youtube.com/watch?v=GPW3CJguUok&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=43)
--- src/components/useEffect/Datafetch.jsx
+++ src/components/useEffect/Datafetch.jsx
@@ -1,25 +1,46 @@
 import React, { useEffect, useState } from 'react';
 
+const loadingMessage = <p>Todo Is Loading</p>;
+
 const Datafetch = () => {
   const [todos, setTodos] = useState(null);
+
+  const [isLodingh, setIsLoding] = useState(true);
+
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(res => {
-        return res.json();
+        if (!res.ok) {
+          throw Error('feching is not sucessfull');
+        } else {
+          return res.json();
+        }
       })
       .then(data => {
         setTodos(data);
+        setIsLoding(false);
+        setError(null);
+      })
+      .catch(error => {
+        setError(error.message);
+        setIsLoding(false);
       });
   }, []);
+
+  const todosElement =
+    todos &&
+    todos.map(todo => {
+      return <p key={todo.id}>{todo.title}</p>;
+    });
 
   return (
     <div>
       <h1>Todos</h1>
-      {todos &&
-        todos.map(todo => {
-          return <p key={todo.id}>{todo.title}</p>;
-        })}
+      {error && <p>{error}</p>}
+      {isLodingh && loadingMessage}
+      {todosElement}
     </div>
   );
 };
