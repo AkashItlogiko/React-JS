50d8f073fdb99b567d3d8c062f492f58df25505f
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-05 00:23:32 +0600

    Done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
- 
+ [react routing | introduction to react routing  ](https://www.youtube.com/watch?v=1_powatXjds&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=60)
+ # react routing | introduction to react routing
+ ## Jokhon amra akta fresh ReactJs project setUp kori ```vite``` ba ```parcel``` (bundeller)diya tokhon amader oi ReactJs project ar moddhe routing ar jonna kono fachility thake na and ai routing fachility take amader ReaactJs project ar moddhe..ai command ar maddhome 'npm install reac t-router-dom' ba amra chaile amader ReactRouter.com ai website ar moddhe theke...o amader ai ReactRouter package install korar command ta copy kore niya ashte pari.
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import { BrowserRouter,Route,Routes } from 'react-router-dom'; //amader ReactJs project ar moddhe Routing ar facility take add korar jonno amra ReactRouter package take install kore niyechi amader ai ReactJs project ar moddhe and install hoye jawar pore ami aikhane react-router-dom theke BrowserRouter , Routes , Route  import korechi
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from"./pages/Contact";
+import Error from './pages/Error';
 import '../src/App.css';
-import { BrowserRouter } from 'react-router-dom';
 const App = () => {
   return (
     <BrowserRouter>
+         <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/blogs' element={<Blogs/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='*' element={<Error/>}/> //akhane ami path='*' aita mane hocche jodi eemon kono route ba url diye amader browser ar moddhe hit kora hoy jei route ba url ta amra create kori nai tokhon oi route ba url ta amader ai astic "*" path ar moddhe chole ashbe and akhane amader element ar moddhe jei Component ta ache oi Component ar moddhe jei content gulo ache ta browser ar moddhe show korbe 
+         </Routes>
     </BrowserRouter>
   );
 };
