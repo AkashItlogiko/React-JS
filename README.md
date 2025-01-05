aefb621b4f9670dc8f3579637cb0b9baca9b36e4
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-06 02:30:52 +0600

    Done

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-[React routing | Navigation and redirect ](https://www.youtube.com/watch?v=DooqgS1JDg0&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=64)
+[React routing | dynamic routing using useParams ](https://www.youtube.com/watch?v=g5B0Vq3jHbA&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=64)
 
 # React routing | Navigation and redirect
 
 ## Jokhon amra akta fresh ReactJs project setUp kori `vite` ba `parcel` (bundeller)diya tokhon amader oi ReactJs project ar moddhe routing ar jonna kono fachility thake na and ai routing fachility take amader ReaactJs project ar moddhe..ai command ar maddhome 'npm install reac t-router-dom' ba amra chaile amader ReactRouter.com ai website ar moddhe theke...o amader ai ReactRouter package install korar command ta copy kore niya ashte pari.
+
+```useParams Hook```
+amra ai useParams Hook ta use kori dynamic routing ar kaj korar jonno.
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; //amader ReactJs project ar moddhe Routing ar facility take add korar jonno amra ReactRouter package take install kore niyechi amader ai ReactJs project ar moddhe and install hoye jawar pore ami aikhane react-router-dom theke BrowserRouter , Routes , Route  import korechi
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
+import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Error from './pages/Error';
 import '../src/App.css';
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:title" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error />} /> //akhane ami path='*' aita mane
         hocche jodi eemon kono route ba url diye amader browser ar moddhe hit
