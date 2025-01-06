5abf706c02963d3d80ad10640a3a2f8d041c2648
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-06 13:09:30 +0600

    Done

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,19 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { blogsData } from '../data';
 const Blog = () => {
-    const {title} =useParams();
-    const[bodyData,setBodydata]=useState("")
-    useEffect(()=>{
-const blogData = blogsData.filter((blog)=>blog.title===title);
-setBodydata(blogData[0].body);
-    },[])
-    return (
-        <div>
-          <h1>{title} page</h1>
-          <p>{bodyData.slice(0,500)}</p>
-          <p>{bodyData.slice(501,5000)}</p>
-        </div>
-    );
+  const { title } = useParams();
+  const [bodyData, setBodydata] = useState('');
+  useEffect(() => {
+    const blogData = blogsData.filter(blog => blog.title === title);
+    console.log(blogData);
+    setBodydata(blogData[0].body);
+  }, []);
+  return (
+    <div>
+      <h1>{title} page</h1>
+      <p>{bodyData.slice(0, 500)}</p>
+      <p>{bodyData.slice(501, 5000)}</p>
+    </div>
+  );
 };
 
 export default Blog;
