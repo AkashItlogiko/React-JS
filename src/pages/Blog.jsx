d1406cdea9ab3ac85acf2ca453288da6ed2747a2
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-07 10:19:52 +0600

    Done

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,15 +1,9 @@
-import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
-import { blogsData } from '../data';
+
 const Blog = () => {
   const { title } = useParams();
   const location = useLocation();
 
-  // const[bodyData,setBodydata]=useState("")
-  //     useEffect(()=>{
-  // const blogData = blogsData.filter((blog)=>blog.title===title);
-  // setBodydata(blogData[0].body);
-  //     },[])
   return (
     <div>
       <h1>{title} page</h1>
