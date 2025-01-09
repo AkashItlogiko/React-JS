76a9adcd00970f23857adadfc77a4e2c79a10d73
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-09 19:20:31 +0600

    Done

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,20 @@
-import React, { memo } from 'react';
+import { array, number } from 'prop-types';
+import React, { memo, useMemo } from 'react';
 
 const Message = ({ numberOfMessage, onHandleDecrement }) => {
   console.log('message');
+
+  const calculatedNumber = useMemo(() => {
+    let number = 0;
+    for (let index = 0; index < 50000; index++) {
+      number++;
+    }
+    return number;
+  }, []);
+
   return (
     <div>
+      <h2>Number:{calculatedNumber}</h2>
       <p>send {numberOfMessage} message</p>
       <button onClick={() => onHandleDecrement()}>
         Decrement Message Number
