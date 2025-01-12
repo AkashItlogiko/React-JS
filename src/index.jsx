649ea675e839e29ab59ac8b4fe0d7d01e39a5cb4
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-12 20:31:29 +0600

    Done

--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,52 @@
-{
-  const { createStore } = require('redux');
+const { createStore } = require('redux');
 
-  const INCREMENT = 'INCREMENT';
-  const DECREMENT = 'DECREME NT';
-  const ADD_USER = 'ADD_USER';
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREME NT';
+const ADD_USER = 'ADD_USER';
 
-  //state
-  const initialCounterState = {
-    count: 0,
+//state
+const initialCounterState = {
+  count: 0,
+};
+const initialUsersState = {
+  users: [{ name: 'Anisul islam' }],
+};
+//action-object-type,payload
+const incrementCounter = () => {
+  return {
+    type: INCREMENT,
   };
-  const initialUsersState = {
-    users: [{ name: 'Anisul islam' }],
+};
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
   };
-  //action-object-type,payload
-  const incrementCounter = () => {
-    return {
-      type: INCREMENT,
-    };
-  };
-  const decrementCounter = () => {
-    return {
-      type: DECREMENT,
-    };
-  };
+};
 
-  // create reducer for counter
+// create reducer for counter
 
-  const counterReducer = (state = initialCounterState, action) => {
-    switch (action.type) {
-      case INCREMENT:
-        return {
-          ...state,
-          count: state.count + 1,
-        };
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
 
-      case DECREMENT:
-        return {
-          ...state,
-          count: state.count - 1,
-        };
-      default:
-        state;
-    }
-  };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      state;
+  }
+};
 
-  const store = createStore(counterReducer);
-  store.subscribe(() => {
-    console.log(store.getState());
-  });
+const store = createStore(counterReducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-  //dispatch action
-  store.dispatch(incrementCounter());
-}
+//dispatch action
+store.dispatch(incrementCounter());
