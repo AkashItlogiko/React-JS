57aa16ffe0cb98f158b39831f7af9c042fe2cb53
AkashItlogiko <akashsahanwu089@gmail.com>
2025-01-14 12:22:29 +0600

    Done

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-# redux | payload
+# redux | Multiple reducers
 
-[ redux | payload](https://www.youtube.com/watch?v=7-Y96jM7xmg&list=PLgH5QX0i9K3pe7Z7ATcyLdUW3grE4Vfld&index=6)
+[ redux | Multiple reducers](https://www.youtube.com/watch?v=MN3-kkwMgxQ&list=PLgH5QX0i9K3pe7Z7ATcyLdUW3grE4Vfld&index=7)
 
 # For Installing Redux on ReactJs Project
 
-`npm install react-redux` kintu amra ai branch ar moddhe ReactJs project ar
-sathe Redux javascript library take use kori nai amra shudhu Redux javascript
-library niye kaj korchi tai amra akhane `npm install redux`command ta chaliye
-amader redux javascript library take install kore niyechi....kintu amra ai
-branch ar moddhe ReactJs project ar sathe Redux javascript library take use kori
-nai amra shudhu Redux javascript library niye kaj korchi tai amra akhane npm
-install reduxcommand ta chaliye amader redux javascript library take install
-kore niyechi....
+- `npm install react-redux`
+
+kintu amra ai branch ar moddhe ReactJs project ar sathe Redux javascript library
+take use kori nai amra shudhu Redux javascript library niye kaj korchi tai amra
+akhane `npm install redux`command ta chaliye amader redux javascript library
+take install kore niyechi....
--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,102 @@
+const { createStore } = require('redux');
+
 //products constants
-const GET_PRODUCTS="GET_PRODUCTS";
-const ADD_PRODUCTS="ADD_PRODUCT";
+const GET_PRODUCTS = 'GET_PRODUCTS';
+const ADD_PRODUCTS = 'ADD_PRODUCT';
+
+//cart constants
+const GET_CART_ITEMS = 'GET_CART_ITEMS';
+const ADD_CART_ITEM = 'ADD_CART_ITEM';
 
 // productReducer
 
-const initialProductState={
-  products:["sugar","salt"],
-  numberofProducts:2,
-}
+const initialProductState = {
+  products: ['sugar', 'salt'],
+  numberofProducts: 2,
+};
 
-const getProducts=()=>{
-  return{
-    type:GET_PRODUCTS,
-  }
-}
+//Cart states
+const initialCartState = {
+  cart: ['sugar'],
+  numberofCartProducts: 1,
+};
 
-const addProduct=(product)=>{
-  return{
-    type:GET_PRODUCTS,
-    payload:product
+//product actions
+
+const getProducts = () => {
+  return {
+    type: GET_PRODUCTS,
+  };
+};
+
+const addProduct = product => {
+  return {
+    type: ADD_PRODUCTS,
+    payload: product,
+  };
+};
+
+//cart actions
+
+const getCart = () => {
+  return {
+    type: GET_CART_ITEMS,
+  };
+};
+
+const addCart = product => {
+  return {
+    type: ADD_CART_ITEM,
+    payload: product,
+  };
+};
+
+//ProductReducer
+const productReducer = (state = initialCartState, action) => {
+  switch (action.type) {
+    case GET_CART_ITEMS:
+      return {
+        ...state,
+      };
+    case ADD_CART_ITEM:
+      return {
+        cart: [...state.cart, action.payload],
+        numberofCartProducts: state.numberofCartProducts + 1,
+      };
+    default:
+      return state;
   }
-}
+};
+//cartReducer
+const cartReducer = (state = initialProductState, action) => {
+  switch (action.type) {
+    case GET_PRODUCTS:
+      return {
+        ...state,
+      };
+    case ADD_PRODUCTS:
+      return {
+        products: [...state.products, action.payload],
+        numberofProducts: state.numberofProducts + 1,
+      };
+    default:
+      return state;
+  }
+};
 
 //cartReducer
+//combinReducers muloto multiple reducers ka hendle korbar jonna used kora hoy
+const rootReducer = combinReducers({
+  productR: productReducer,
+  cartR: cartReducer,
+});
+
+//store
+const store = createStore(rootReducer);
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(getProducts());
+store.dispatch(addProduct('pen'));
+store.dispatch(getCart());
+store.dispatch(addCart('pen'));
